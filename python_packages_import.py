2c951e2409b8c996409982c4fd103b56c435b14d
Francisco Mendoza Torres <mentofran@gmail.com>
2019-10-12 08:58:32 -0500

    Import of python packages I use

---
+++ python_packages_import.py
@@ -0,0 +1,17 @@
+import os
+import sys
+import re
+import json
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from beauti
+import pickle
+from sqlalchemy.engine import create_engine
+import requests
+from flask import Flask, request, jsonify
+import scikit-learn as sklearn
+import tensorflow as tf
+from keras import models, layers
+from keras.utils import to_categorical
