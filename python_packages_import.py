f44cb54c3261005b8e4cb51428cc43af04d23e18
Francisco Mendoza Torres <mentofran@gmail.com>
2019-11-01 20:24:56 -0600

    Update python_packages_import.py

--- python_packages_import.py
+++ python_packages_import.py
@@ -6,9 +6,11 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-from beauti
 import pickle
 from sqlalchemy.engine import create_engine
+from elasticsearch import Elasticsearch
+from elasticsearch.helpers import bulk
+from elasticsearch_dsl import Document, Text, Date
 import requests
 from flask import Flask, request, jsonify
 import scikit-learn as sklearn
